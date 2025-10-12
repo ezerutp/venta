a0f61d52fd0872300ee68c5bbb34951f8c8b8777
ezerutp <u22320821@utp.edu.pe>
2025-10-12 09:48:49 -0500

    fix: Actualización de iconos a color blanco en la barra lateral y el encabezado (usamos material design icons)

--- src/main/java/pe/olpesa/venta/entities/Cliente.java
+++ src/main/java/pe/olpesa/venta/entities/Cliente.java
@@ -14,7 +14,6 @@ import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
